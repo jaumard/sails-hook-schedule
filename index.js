813abff9fc7970fe55c050e545b0f17602bc277e
jaumard <ioquoo3H>
2015-03-28 23:31:03 +0100

    Base hook

--- index.js
+++ index.js
@@ -21,14 +21,21 @@ module.exports = function (sails)
 		// Run when sails loads-- be sure and call `next()`.
 		initialize : function (next)
 		{
-			sails.after('hook:orm:loaded', function ()
+			if (sails.config.schedule == undefined)
 			{
-				Object.keys(sails.config.schedule).forEach(function (key)
+				sails.log.error("Schedule module need config/schedule.js file");
+			}
+			else
+			{
+				sails.after('hook:orm:loaded', function ()
 				{
-					var val = sails.config.schedule[key];
-					schedule.scheduleJob(key, val);
+					Object.keys(sails.config.schedule).forEach(function (key)
+					{
+						var val = sails.config.schedule[key];
+						schedule.scheduleJob(key, val);
+					});
 				});
-			});
+			}
 			return next();
 		}
 	};
